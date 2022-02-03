cdca06ad50d32073850a8efcdadeb7995c896426
Sarsfield Zelda <sazelda@oa-d1.msk.21-school.ru>
2022-02-03 15:19:50 +0300

    help some

--- ft_philosophers.h
+++ ft_philosophers.h
@@ -5,12 +5,14 @@
 # include <stdlib.h>
 # include <sys/time.h>
 #include <unistd.h>
+# include <stdbool.h>
 # include <stdio.h> //FIX_ME
 
 typedef struct s_philosof
 {
+	struct s_table	*table;
 	int			name;
-	long long			time_eat;
+	long long	time_eat;
 	long long	time_sleap;
 	long long	time_death;
 	long long	last_eat;
@@ -22,6 +24,7 @@ typedef struct s_table
 {
 	t_philos		philosopher;
 	pthread_mutex_t	*forks;
+	bool			stop;
 }	t_table;
 
 pthread_mutex_t entry_point = PTHREAD_MUTEX_INITIALIZER;
--- main.c
+++ main.c
@@ -6,11 +6,12 @@
 /*   By: sazelda <sazelda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 11:38:28 by sazelda           #+#    #+#             */
-/*   Updated: 2022/01/26 00:39:15 by sazelda          ###   ########.fr       */
+/*   Updated: 2022/02/03 15:17:03 by sazelda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_philosophers.h"
+pthread_t 		*threads;
 
 pthread_mutex_t *ft_create_forks(int count)
 {
@@ -86,35 +87,42 @@ void	*live(void *args)
 	long time1 = 0;
 	long time_start_think = 0;
 	int i = 0;
-	while (1)
+	while (!table->stop)
 	{
-		//pthread_mutex_lock(&entry_point);
 	
-			
-			//pthread_mutex_lock(&entry_point);
+			if (table->stop)
+			break ;
 			if (table->philosopher.right_fork > table->philosopher.left_fork)
 			{
 				pthread_mutex_lock(&table->forks[table->philosopher.left_fork]);
 				gettimeofday(&tv, NULL);
 				time = tv.tv_sec * 1000 + tv.tv_usec/1000;
+				pthread_mutex_lock(&entry_point);
 				printf("%lld %d has taken a fork\n",  time, table->philosopher.name);
+				pthread_mutex_unlock(&entry_point);
 			
 				pthread_mutex_lock(&table->forks[table->philosopher.right_fork]);
 				gettimeofday(&tv, NULL);
 				time = tv.tv_sec * 1000 + tv.tv_usec/1000;
+				pthread_mutex_lock(&entry_point);
 				printf("%lld %d has taken a fork\n",  time, table->philosopher.name);
+				pthread_mutex_unlock(&entry_point);
 			}
 			else
 			{
 				pthread_mutex_lock(&table->forks[table->philosopher.right_fork]);
 				gettimeofday(&tv, NULL);
 				time = tv.tv_sec * 1000 + tv.tv_usec/1000;
+				pthread_mutex_lock(&entry_point);
 				printf("%lld %d has taken a fork\n",  time, table->philosopher.name);
+				pthread_mutex_unlock(&entry_point);
 			
 				pthread_mutex_lock(&table->forks[table->philosopher.left_fork]);
 				gettimeofday(&tv, NULL);
 				time = tv.tv_sec * 1000 + tv.tv_usec/1000;
+				pthread_mutex_lock(&entry_point);
 				printf("%lld %d has taken a fork\n",  time, table->philosopher.name);
+				pthread_mutex_unlock(&entry_point);
 			}
 		//	pthread_mutex_unlock(&entry_point);
 			
@@ -122,16 +130,25 @@ void	*live(void *args)
 				table->philosopher.last_eat = time;
 			//time_think = time - time1;
 		
-
 		
+		if (table->stop)
+			break ;
 		printf("from eat %dms, for %d\n", time - table->philosopher.last_eat, table->philosopher.name);	
 		if ((time - table->philosopher.last_eat) <= table->philosopher.time_death)
-			printf("%lld %d is eating\n",  time, table->philosopher.name);
+		{
+			pthread_mutex_lock(&entry_point);
+			printf("%lld %d is eating\n",  time, table->philosopher.name);	
+			pthread_mutex_unlock(&entry_point);
+		}
 		else
 		{
+			pthread_mutex_lock(&entry_point);
 			printf("%lld %d died\n",  time, table->philosopher.name);
+			table->philosopher.name = 0;
 			return (NULL);
 		}
+		if (table->stop)
+			break ;
 		castom_usleep(table->philosopher.time_eat);
 
 			if (table->philosopher.right_fork > table->philosopher.left_fork)
@@ -144,26 +161,75 @@ void	*live(void *args)
 				pthread_mutex_unlock(&table->forks[table->philosopher.left_fork]);
 				pthread_mutex_unlock(&table->forks[table->philosopher.right_fork]);
 			}
-		
+		if (table->stop)
+			break ;
 		table->philosopher.last_eat = time;
 		time += table->philosopher.time_eat;
-			
+			pthread_mutex_lock(&entry_point);
 		printf("%lld %d is sleeping\n", time, table->philosopher.name);
+		pthread_mutex_unlock(&entry_point);
 		//usleep(table->philosopher.time_sleap * 1000);
 		castom_usleep(table->philosopher.time_sleap);
 		time += table->philosopher.time_sleap;
+		if (table->stop)
+			break ;
+		pthread_mutex_lock(&entry_point);
 		printf("%lld %d is thinking\n",  time, table->philosopher.name);
+		pthread_mutex_unlock(&entry_point);
 		i++;
 	}
 	return (NULL);
 }
+pthread_mutex_t *forks;
+void	*moni(void *args)
+{
+	t_table			*table;
+	int i = 0;
+	struct timeval tv;
+	long time;
+	
+	table = (t_table *)args;
+	printf("MONI\n");
+	gettimeofday(&tv, NULL);
+	time = tv.tv_sec * 1000 + tv.tv_usec/1000;
+	while (!table[i].stop)
+	{
+		gettimeofday(&tv, NULL);
+		time = tv.tv_sec * 1000 + tv.tv_usec/1000;
+		//printf("find %ld > %ld\n", time - table[i].philosopher.last_eat, table[i].philosopher.time_death);
+		if (time - table[i].philosopher.last_eat > table[i].philosopher.time_death && table[i].philosopher.last_eat != 0)
+		{
+			table[i].stop = true;
+			if (table[i].stop)
+				break;
+		}
+		i++;
+		if (i == 4)
+			i = 0;
+	}
+	printf("%lld %d died\n",  time, table->philosopher.name);
+	pthread_mutex_destroy(&entry_point);
+	while (i < 4)
+	{
+		table[i].stop = true;
+		i++;
+	}
+	i = 0;
+	while (i < 4)
+	{
+		pthread_mutex_destroy(&forks[i]);
+		i++;
+	}
+	return(NULL);
+}
 
 int	main(int argc, char **argv)
 {
-	pthread_mutex_t *forks;
+	
 	t_philos		*philosophers;
 	t_table			*table;
-	pthread_t 		*threads;
+	
+	pthread_t 		monitor;
 	int				i;
 	struct timeval tv;
 
@@ -179,19 +245,26 @@ int	main(int argc, char **argv)
 	{
 		table[i].philosopher = philosophers[i];
 		table[i].forks = forks;
+		table[i].stop = false;
 		i++;
 	}
 	i = 0;
+	pthread_create(&monitor, NULL, moni, table);
+	//pthread_detach(monitor);
 	while (i < ft_atoi(argv[1]))
 	{
 		table[i].philosopher.last_eat = 0;
 		pthread_create(&threads[i], NULL, live, &table[i]);
+		
 		i++;
 	}
-	i = 0;
+	pthread_detach(monitor);
+i = 0;
+	//pthread_create(&monitor, NULL, moni, table);
+	//pthread_detach(monitor);
 	while (i < ft_atoi(argv[1]))
 	{
-		pthread_join(threads[i], NULL);
+		pthread_join(threads[i],NULL);
 		i++;
 	}
 	return (0);
